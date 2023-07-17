0f3f9825dc76a9459ef0d8a0a73a930f1e450fac
macray <macraykorir254@gmail.com>
2023-07-17 19:45:58 +0300

    React Component

--- src/Article.js
+++ src/Article.js
@@ -5,5 +5,52 @@ function Article() {
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
+// function Comment() {
+//   return <div>Naturally, I agree with this article.</div>;
+// }
+// function App() {
+//   return (
+//     <div>
+//       <Article />
+//       <Comment />
+//     </div>
+//   );
+// }
+// function Article() {
+//   return (
+//     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+//   );
+// }
+// function Comment() {
+//   return <div>Naturally, I agree with this article.</div>;
+// }
+// function article() {
+//   return (
+//     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+//   );
+// }
+// function App() {
+//   return (
+//     <div>
+//       <article />
+//     </div>
+//   );
+// }
 
+// // returns these DOM elements:
+// // <div>
+// //  <article />
+// // </div>
+// function App() {
+//   return (
+//     <div>
+//       <Article />
+//     </div>
+//   );
+// }
+
+// returns these DOM elements:
+// <div>
+//  <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+// </div>
 export default Article;
